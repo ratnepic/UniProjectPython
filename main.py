cb8cfbe2a1d4e2da4eab07a87f79ae4ebb44231e
Матвей Порынов <matveynothing@gmail.com>
2023-05-27 00:24:28 +0300

    do EVERYTHING!!!

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+from MainMenuScene import MainMenuScene
+from GameScene import GameScene
+from GameOverScene import GameOverScene
+import pygame as pg
+
+
 def main():
-    pass
+    pg.init()
+    screen_width = 1280
+    screen_height = 720
+    screen = pg.display.set_mode((screen_width, screen_height))
+
+    running = True
+    while running:
+        main_menu = MainMenuScene(screen)
+        game_scene = GameScene(screen)
+        game_over_screen = GameOverScene(screen)
+
+        main_menu.open()
+
+        if main_menu.code == 1:
+            game_scene.open()
+        elif main_menu.code == 0:
+            running = False
+
+        if game_scene.code == 2:
+            continue
+        elif game_scene.code == 1:
+            game_over_screen.open()
+        elif game_scene.code == 0:
+            running = False
+
+        if game_over_screen.code == 1:
+            continue
+        elif game_over_screen.code == 0:
+            running = False
+
+    pg.quit()
 
 
 if __name__ == '__main__':
